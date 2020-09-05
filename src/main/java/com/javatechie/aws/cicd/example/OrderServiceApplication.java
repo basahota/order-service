df13963cb6654e07e71d1609ab82f159324b17d5
basahota <techbasanta757@gmail.com>
2020-09-05 18:23:52 +0530

    first commit

---
+++ src/main/java/com/javatechie/aws/cicd/example/OrderServiceApplication.java
@@ -0,0 +1,29 @@
+package com.javatechie.aws.cicd.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/orders")
+public class OrderServiceApplication {
+
+    @Autowired
+    private OrderDao orderDao;
+
+    @GetMapping
+    public List<Order> fetchOrders() {
+        return orderDao.getOrders();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+
+}
